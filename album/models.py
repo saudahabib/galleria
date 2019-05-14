222703f0a77ca0ff9666b3eb9a5f3019bd7f14e4
saudahabib <saudababs00@gmail.com>
2019-05-14 14:22:02 +0300

    wrote more tests for application

--- album/models.py
+++ album/models.py
@@ -3,14 +3,23 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     category = models.CharField(max_length=30)
+    def save_category(self):
+        self.save()
+
+
 
     def __str__(self):
         return self.name
 
 class Location(models.Model):
     location=models.CharField(max_length=30)
+    def save_location(self):
+        self.save()
+
+
     def __str__(self):
         return self.image_name
+
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/', blank = True)
@@ -18,6 +27,20 @@ class Image(models.Model):
     image_description = models.CharField(max_length=30)
     location=models.ForeignKey(Location)
     category=models.ForeignKey(Category)
+
+    '''function to save'''
+    def save_image(self):
+        self.save()
+
+    '''function to delete'''
+    def delete_image(self):
+        to_delete=Image.objects.all()
+        to_delete.delete()
+
+    '''function to display'''
+    def display_images(self):
+        all_images=Image.objects.all()
+        return all_images
 
     def __str__(self):
         return self.image_name
