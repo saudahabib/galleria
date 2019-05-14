0afa1682cc8b42d5e640edcfa2067b2a66ea5d8a
saudahabib <saudababs00@gmail.com>
2019-05-14 13:08:52 +0300

    wrote first tests for all models, God is good!

--- album/models.py
+++ album/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    location=models.CharField(max_length=30)
+    def __str__(self):
+        return self.image_name
+
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/', blank = True)
     image_name = models.CharField(max_length=30)
     image_description = models.CharField(max_length=30)
+    location=models.ForeignKey(Location)
+    category=models.ForeignKey(Category)
 
     def __str__(self):
         return self.image_name
+    class Meta:
+        ordering = ['image_name']
--- album/tests.py
+++ album/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from .models import Image, Location, Category
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.img1=Image(image="x.jpg",image_name="scenery",image_description="an image test")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.img1,Image))
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.loc1=Location(location="Mombasa")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc1,Location))
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.cat1=Category(category="food")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cat1,Category))
