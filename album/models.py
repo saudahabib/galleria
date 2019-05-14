c5d92cfb07bf0315a994060cc0371a81fea9d4ca
saudahabib <saudababs00@gmail.com>
2019-05-14 12:26:50 +0300

    enable adding images to site

--- album/models.py
+++ album/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/', blank = True)
+    image_name = models.CharField(max_length=30)
+    image_description = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.image_name
